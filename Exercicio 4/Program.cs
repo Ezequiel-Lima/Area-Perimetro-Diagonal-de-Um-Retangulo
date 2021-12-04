94e1706f1aa45a27c3cff5c69a6371c3d38e2452
Quiel <Quiel@DESKTOP-QPM4FGN>
2021-12-04 09:22:26 -0300

    Add project files.

---
+++ Exercicio 4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre com a largura e altura do retângulo:");
+            double largura = double.Parse(Console.ReadLine());
+            double altura = double.Parse(Console.ReadLine());
+
+            Retangulo retangulo = new Retangulo(largura, altura);
+
+            Console.WriteLine($"{retangulo}");
+
+            Console.WriteLine("\nPressione qualquer tecla para finalizar");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Exercicio 4/Retangulo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Exercicio_4
+{
+    public class Retangulo
+    {
+        public double Largura { get; set; }
+        public double Altura { get; set; }
+
+        public Retangulo(double largura, double altura)
+        {
+            Largura = largura;
+            Altura = altura;
+        }
+
+        public double Area()
+        {
+            return Largura * Altura;    
+        }
+
+        public double Perimetro()
+        {
+            return 2 * (Largura + Altura);
+        }
+
+        public double Diagonal()
+        {
+            return Math.Sqrt(Math.Pow(Altura, 2) + Math.Pow(Largura, 2));
+        }
+
+        public override string ToString()
+        {
+            return "AREA: " +
+                Area().ToString("F2") +
+                "\nPERÍMETRO: " +
+                Perimetro().ToString("F2") +
+                "\nDIAGONAL: " +
+                Diagonal().ToString("F2");
+        }
+    }
+}
